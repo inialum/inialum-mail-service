e3b04bfa5f15ecddef708b3362f61e337e4fbfb7
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2023-11-12 00:50:26 +0900

    feat: Make it possible to handle error in token generator

--- src/utils/createToken.ts
+++ src/utils/createToken.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { sign } from 'jws'
 import '@Root/env.ts'
 
@@ -5,12 +6,20 @@ const payload = {
   service_name: 'inialum-mail-service',
   created_at: new Date().getTime(),
 }
-const secret = process.env.TOKEN_SECRET
-const token = sign({
-  header: {
-    alg: 'HS256',
-  },
-  payload,
-  secret,
-})
-console.log(`token: ${token}`)
+try {
+  const secret = process.env.TOKEN_SECRET
+  const token = sign({
+    header: {
+      alg: 'HS256',
+    },
+    payload,
+    secret,
+  })
+  console.log(`token: ${token}`)
+} catch (e) {
+  if (e instanceof Error) {
+    console.log('Failed to create token:', e.message)
+  } else {
+    console.log(e)
+  }
+}
