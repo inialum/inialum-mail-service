2b14ca16217d4ba6fa6579104f7a4e6a2ef5a485
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2023-11-13 01:04:36 +0900

    feat: Make it possible to send emails via Amazon SES

---
+++ src/lib/mail/ses.test.ts
@@ -0,0 +1,58 @@
+import {
+  SESv2Client,
+  type SESv2ClientConfig,
+  SendEmailCommand,
+} from '@aws-sdk/client-sesv2'
+import { mockClient } from 'aws-sdk-client-mock'
+
+import { SESApiError } from '@/lib/errors'
+
+import { type Mail } from '@/types/Mail'
+
+import { sendEmailWithSES } from './ses'
+
+const SESv2Mock = mockClient(SESv2Client)
+beforeEach(() => {
+  SESv2Mock.reset()
+})
+
+describe('sendEmailWithSES', () => {
+  const mailConfig: Mail = {
+    fromAddress: 'test@example.com',
+    toAddress: 'to@example.com',
+    subject: 'Test',
+    body: 'Hello world',
+  }
+
+  const credentials: SESv2ClientConfig['credentials'] = {
+    accessKeyId: 'dummyKey',
+    secretAccessKey: 'dummySecret',
+  }
+
+  test('Should send mail via SES with no errors', async () => {
+    SESv2Mock.on(SendEmailCommand).resolves({
+      $metadata: {
+        httpStatusCode: 200,
+      },
+    })
+    const result = await sendEmailWithSES(mailConfig, credentials)
+
+    expect(result).toStrictEqual({
+      $metadata: {
+        httpStatusCode: 200,
+      },
+    })
+  })
+
+  test('Should throw SESApiError', async () => {
+    SESv2Mock.on(SendEmailCommand).resolves({
+      $metadata: {
+        httpStatusCode: 400,
+      },
+    })
+
+    await expect(() =>
+      sendEmailWithSES(mailConfig, credentials),
+    ).rejects.toThrowError(new SESApiError('Failed to send email via SES'))
+  })
+})
