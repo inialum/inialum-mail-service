244356ac518fdd49a129775f18bf4cede10a1603
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2023-11-14 00:55:30 +0900

    feat: Define validation schema for API

---
+++ src/lib/api/v1/schema/send.ts
@@ -0,0 +1,80 @@
+import { z } from '@hono/zod-openapi'
+
+/**
+ * Schema for request of POST /send
+ */
+export const SendApiRequestSchemaV1 = z.object({
+  to: z.string().email('Invalid email address').openapi({
+    example: 'to@example.com',
+  }),
+  subject: z
+    .string({
+      required_error: 'This field is required',
+    })
+    .openapi({
+      example: 'This is a subject',
+    }),
+  body: z
+    .string({
+      required_error: 'This field is required',
+    })
+    .openapi({
+      example: 'This is a body',
+    }),
+})
+
+/**
+ * Schema for response of POST /send
+ */
+export const SendApiResponseSchemaV1 = z.object({
+  status: z.string().openapi({
+    example: 'ok',
+  }),
+})
+
+/**
+ * Schema for Zod validation error (internal use)
+ */
+const ZodValidationError = z.object({
+  code: z.string(),
+  expected: z.string(),
+  received: z.string(),
+  path: z.string().array(),
+  message: z.string(),
+})
+
+/**
+ * Schema for 400 error response of POST /send
+ */
+export const SendApi400ErrorSchemaV1 = z.object({
+  message: z.string().openapi({
+    example: 'Invalid request body',
+  }),
+  issues: ZodValidationError.array().openapi({
+    example: [
+      {
+        code: 'invalid_type',
+        expected: 'string',
+        received: 'undefined',
+        path: ['to'],
+        message: 'Required',
+      },
+      {
+        code: 'invalid_type',
+        expected: 'string',
+        received: 'undefined',
+        path: ['subject'],
+        message: 'This field is required',
+      },
+    ],
+  }),
+})
+
+/**
+ * Schema for 500 error response of POST /send
+ */
+export const SendApi500ErrorSchemaV1 = z.object({
+  message: z.string().openapi({
+    example: 'Failed to send email via SES',
+  }),
+})
