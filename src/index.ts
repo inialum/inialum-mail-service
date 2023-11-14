9c16422ee37b36759136233d21e0ce2237dbf96d
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2023-11-14 23:27:10 +0900

    feat: Configure JWT auth for API

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
+import { env } from 'hono/adapter'
+import { jwt } from 'hono/jwt'
 
 import { api } from '@/routes/api'
 
 const app = new OpenAPIHono()
+
+app.use('/api/*', async (c, next) => {
+  const { TOKEN_SECRET } = env<{ TOKEN_SECRET: string }>(c)
+  const auth = jwt({
+    secret: TOKEN_SECRET,
+  })
+  return await auth(c, next)
+})
+
 app.get('/', (c) => c.text('Hello Hono!'))
 app.route('/api', api)
 app.doc('/docs/v1', {
