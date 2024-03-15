3d874a275b0ab1fd69b8d850420cb7eb85cab18f
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-03-16 02:20:59 +0900

    fix: Fix typo in DEFAULT_MAIL_FROM_NAME constant

--- src/constants/mail.ts
+++ src/constants/mail.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_MAIL_FROM_NAME = 'INIAD同窓会 「INIALUM」'
+export const DEFAULT_MAIL_FROM_NAME = 'INIAD同窓会「INIALUM」'
 
 export const LOCAL_SES_API_ENDPOINT = 'http://localhost:8005'
