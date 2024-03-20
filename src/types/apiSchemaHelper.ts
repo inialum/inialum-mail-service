58a5395898f9a9391e34799a6fa73d087a2a2bcf
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-03-20 18:33:13 +0900

    chore: Define types to use generated types easily

---
+++ src/types/apiSchemaHelper.ts
@@ -0,0 +1,31 @@
+import { type UnionToIntersection, type Get } from 'type-fest'
+
+
+import { type paths } from '@/types/generated/schema'
+
+const HTTP_STATUS_CODES = [200, 400, 401, 404, 500] as const
+
+export type UrlPaths = keyof paths
+
+export type HttpMethods = keyof UnionToIntersection<paths[UrlPaths]>
+
+export type HttpMethodsFilteredByPath<Path extends UrlPaths> = HttpMethods &
+  keyof UnionToIntersection<paths[Path]>
+
+export type HttpStatusCode = (typeof HTTP_STATUS_CODES)[number]
+
+export type RequestParameters<
+  Path extends UrlPaths,
+  Method extends HttpMethods,
+> = Get<paths, `${Path}.${Method}.parameters.query`>
+
+export type RequestData<
+  Path extends UrlPaths,
+  Method extends HttpMethods,
+> = Get<paths, `${Path}.${Method}.requestBody.content.application/json`>
+
+export type ResponseData<
+  Path extends UrlPaths,
+  Method extends HttpMethods,
+  Status extends HttpStatusCode,
+> = Get<paths, `${Path}.${Method}.responses.${Status}.content.application/json`>
