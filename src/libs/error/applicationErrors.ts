03169894c4fb39accff7cb8243b0916d52d80d89
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-10-13 00:56:23 +0900

    chore: Define SendGridApiError

--- src/libs/error/applicationErrors.ts
+++ src/libs/error/applicationErrors.ts
@@ -21,6 +21,21 @@ export class SESApiError extends Error {
 }
 
 /**
+ * This error is thrown when SendGrid API returns an error.
+ */
+export class SendGridApiError extends Error {
+  static {
+    // biome-ignore lint/complexity/noThisInStatic: For avoiding TypeError
+    this.prototype.name = 'SendGridApiError'
+  }
+
+  // biome-ignore lint/complexity/noUselessConstructor: This constructor is necessary to set the name of the error
+  constructor(message: string, options?: ErrorOptions) {
+    super(message, options)
+  }
+}
+
+/**
  * This error is thrown when fetchApi fails to fetch the API
  */
 export class FetchApiError extends Error {
