65851aa6efa964fce884fd4d605db5656ba497df
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-10-13 01:30:40 +0900

    feat: Create sendEmailWithSendGrid function

---
+++ src/libs/mail/sendgrid.ts
@@ -0,0 +1,38 @@
+import { encodeWord } from 'libmime'
+
+import sgMail, { ResponseError, type MailDataRequired } from '@sendgrid/mail'
+
+import { DEFAULT_MAIL_FROM_NAME } from '@/constants/mail'
+
+import { SendGridApiError } from '@/libs/error/applicationErrors'
+
+import type { Mail } from '@/types/Mail'
+
+export const sendEmailWithSendGrid = async (
+  { fromAddress, toAddresses, subject, body: { text, html } }: Mail,
+  token: string,
+) => {
+  const from = `${encodeWord(DEFAULT_MAIL_FROM_NAME)} <${fromAddress}>`
+
+  const params: MailDataRequired = {
+    to: toAddresses,
+    from,
+    subject,
+    text,
+    html,
+  }
+
+  try {
+    sgMail.setApiKey(token)
+    await sgMail.sendMultiple(params)
+  } catch (error) {
+    throw new SendGridApiError(
+      error instanceof ResponseError
+        ? error.message
+        : `Failed to send email via SendGrid\nTo: ${toAddresses.join(', ')}`,
+      {
+        cause: error,
+      },
+    )
+  }
+}
