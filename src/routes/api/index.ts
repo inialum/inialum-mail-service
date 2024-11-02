790cefe20852f0bac057f8d5fcea4cd6a6ba52e3
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2024-11-03 02:28:05 +0900

    chore: Refactor error handling

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -4,3 +4,22 @@ import { apiV1 } from './v1'
 
 export const api = new OpenAPIHono()
 api.route('/v1', apiV1)
+
+api.onError((error, c) => {
+	console.error(error)
+
+	if (error instanceof Error) {
+		return c.json(
+			{
+				message: error.message,
+			},
+			500,
+		)
+	}
+	return c.json(
+		{
+			message: 'Internal server error',
+		},
+		500,
+	)
+})
