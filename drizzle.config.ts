8b7f4deeff790d45e5c118eb598a093b80ceb089
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2025-06-07 21:17:53 +0900

    chore: Add drizzle configuration for database migrations and environment variables

---
+++ drizzle.config.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv'
+import { defineConfig } from 'drizzle-kit'
+
+import { getEnv } from '@/utils/getEnv'
+
+dotenv.config({ path: './.dev.vars' })
+
+const accountId = getEnv(process.env.CLOUDFLARE_ACCOUNT_ID)
+const databaseId = getEnv(process.env.PRODUCTION_MAIL_SERVICE_LOG_DB_ID)
+const token = getEnv(process.env.CLOUDFLARE_D1_TOKEN)
+
+export default defineConfig({
+	schema: './src/libs/db/schema.ts',
+	out: './migrations',
+	dialect: 'sqlite',
+	driver: 'd1-http',
+	dbCredentials: {
+		accountId,
+		databaseId,
+		token,
+	},
+	verbose: true,
+	strict: true,
+})
