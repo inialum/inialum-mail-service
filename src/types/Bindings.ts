c72455aee800ea429f3eaea63899e7fc1fe3cc65
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2025-09-01 00:23:15 +0900

    chore: Add SMTP configuration to environment variables and update type definitions

--- src/types/Bindings.ts
+++ src/types/Bindings.ts
@@ -2,9 +2,4 @@ import type { EnvironmentType } from '@inialum/error-notification-service-hono-m
 
 export type Bindings = {
 	ENVIRONMENT: EnvironmentType
-	// SMTP configuration
-	SMTP_HOST?: string
-	SMTP_PORT?: string
-	SMTP_USER?: string
-	SMTP_PASS?: string
 } & CloudflareBindings
